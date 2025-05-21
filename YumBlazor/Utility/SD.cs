63fe4735296db3be92609fd95c511fa7e60bc85a
Neelam Verma <neelam.vma11@gmail.com>
2025-05-21 16:50:13 +0530

    section-14 Authentication

---
+++ YumBlazor/Utility/SD.cs
@@ -0,0 +1,9 @@
+﻿namespace YumBlazor.Utility
+{
+    public static class SD
+    {
+        public static string Role_Admin = "Admin";
+        public static string Role_Customer = "Customer";
+
+    }
+}
